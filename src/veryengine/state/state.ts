890767e5b616229cdd59aa00b20cd386106f0ecb
Eric.J <jxl594@gmail.com>
2019-04-23 18:58:26 +0800

    add dist folder to git

--- src/veryengine/state/state.ts
+++ src/veryengine/state/state.ts
@@ -3,7 +3,6 @@ import { VE_StateAction } from "./stateAction";
 import { VE_AssociatedState } from "./associatedState";
 
 export class VE_State {
-
   public get Fsm(): VE_Fsm {
     return this._fsm;
   }
@@ -12,7 +11,7 @@ export class VE_State {
   public get Value(): string {
     return this._value;
   }
-  private _value: string = '空状态';
+  private _value: string = "空状态";
 
   private _stateActions: VE_StateAction[] = [];
   private _assocaitedStates: VE_AssociatedState[] = [];
@@ -22,8 +21,7 @@ export class VE_State {
   }
   private _isSequence: boolean = false;
 
-
-  constructor(fsm: VE_Fsm, state_value: string = '空状态') {
+  constructor(fsm: VE_Fsm, state_value: string = "空状态") {
     this._fsm = fsm;
     this._value = state_value;
   }
@@ -32,14 +30,8 @@ export class VE_State {
     this._value = state_value;
   }
 
-  addAction(state_action: VE_StateAction) : void {
+  addAction(state_action: VE_StateAction): void {
     this._stateActions.push(state_action);
-    BABYLON.Mesh.bind
+    BABYLON.Mesh.bind;
   }
-  
-
-
-
-
-
 }
